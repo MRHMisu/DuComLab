12094a13a5b1d7ecad4e808af369b29dc00cdef2
MRHMisu <md.rakib.hossain.19@ucl.ac.uk>
2015-02-26 19:28:16 -0800

    desktop app works

---
+++ DuComLab-Desktop-App/DUComLab-Desktop-App/IClientServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using CyberCenterClient.CyberCenterServices;
+namespace CyberCenterClient
+{
+   public interface IClientServices
+   {
+       bool IsValidUser(string CardNumber, string PinNumber);
+       bool IsActive(string CardNumber);
+       void ToBeInActive(string CardNumber);
+       void InsertIntoActiveSession(string CardNumber,string PcName);
+       int getCardUsageIdAfterInsertingStartingTime(string CardNumber, string PcName);
+       int getUsingTime(string CardNumber);
+       void UpdateFinishingTime(int CardUsageId);
+       bool IsCardExperiedByDate(string CardNumber);
+       DataSet ViewCardUsage(string CardNumber);
+   }
+}
