0b0306825c9f79cff18dab8aa347e1264161afd9
MRHMisu <md.rakib.hossain.19@ucl.ac.uk>
2015-03-19 12:56:29 -0700

    service runnable in barckground

---
+++ DuComLab-WCF-Service/Hosting-Service-As-Background-Services/CyberCenterWindowsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+
+namespace HostingInWindowServices
+{
+    public partial class CyberCenterWindowsService : ServiceBase
+    {
+        private ServiceHost MyHost;
+        public CyberCenterWindowsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MyHost = new ServiceHost(typeof(CyberCenter.ClientServices));
+            MyHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            MyHost.Close();
+        }
+    }
+}
